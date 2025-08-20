648ba6720e3d6b8223ec8031cd0193a34d80b8c4
Michal Lefler <michallefler@Michals-MacBook-Air.local>
2025-08-20 15:18:36 +0300

    Initial commit

---
+++ src/main/java/com/michal/demo/controller/HelloController.java
@@ -0,0 +1,14 @@
+// src/main/java/com/example/demo/controller/HelloController.java
+package com.michal.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello, Railway!";
+    }
+}
